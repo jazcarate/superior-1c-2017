815b65b37ee7463974cc7c65fbd711a43c9a3d85
Joaquin "Florius" Azcarate <j@florius.com.ar>
2017-06-03 22:21:07 -0300

    GUIficación

--- ASIC.m
+++ ASIC.m
@@ -3,16 +3,6 @@ pkg load signal;
 
 clear;
 
-function transfer = cargarTF
-  opcion = menu ("Forma de insercion de funcion de transferencia", "Coeficientes", "Polos/Ceros");
-  switch (opcion)
-    case 1
-      transfer = cargarCoeficientes();
-    case 2
-      transfer = cargarPolosCeros();
-  endswitch
-endfunction
-
 function retval = cargarCoeficientes
   disp("El formato es [ #CoeficienteDeMayorGrado, #CoeficienteDeproximoGrado, ..., #CoeficienteTerminoIndependiente]");
   
@@ -33,39 +23,103 @@ function retval = cargarPolosCeros
   retval = tf({num}, {den});
 endfunction
 
-#transfer = cargarTF()
-transfer = tf({[1]}, {[-4, -4]});
+function transfer = cargarTF
+  disp("Ingresar una funcion de transferencia")
+  opcion = menu ("Forma de insercion de funcion de transferencia", "Coeficientes", "Polos/Ceros");
+  switch (opcion)
+    case 1
+      transfer = cargarCoeficientes();
+    case 2
+      transfer = cargarPolosCeros();
+  endswitch
+endfunction
+transfer = cargarTF()
 
+function [ceros, polos, ganancia] = obtenerPolosyCeros(transfer)
+     [num, den] =  tfdata (transfer, "vector");
+     [ceros, polos, ganancia] = tf2zp(num, den);
+endfunction
 
-#1. Obtener la expresión de la función transferencia.
-  transfer
-#2. Indicar Polos.
- [num, den] =  tfdata (transfer, "vector");
- [ceros, polos, ganancia] =  tf2zp(num, den);
- disp("Polos:");
+function opcion1(transfer)
+ transfer
+endfunction
+
+function opcion2(transfer)
+ [ceros, polos, ganancia] =  obtenerPolosyCeros(transfer);
  disp(polos);
- 
-#3. Indicar Ceros.
- [num, den] =  tfdata (transfer, "vector");
- [ceros, polos, ganancia] =  tf2zp(num, den);
- disp("Ceros:");
+endfunction
+
+function opcion3(transfer)
+ [ceros, polos, ganancia] =  obtenerPolosyCeros(transfer);
  disp(ceros);
-#4. Marcar ganancia de la función.
+endfunction
+
+function opcion4(transfer)
+ [ceros, polos, ganancia] =  obtenerPolosyCeros(transfer);
+ disp(ganancia);  
+endfunction
+
+function opcion5(transfer)
  [num, den] =  tfdata (transfer, "vector");
- [ceros, polos, ganancia] =  tf2zp(num, den);
- disp("Ganancia:");
- disp(ganancia);
-#5. Obtener expresión con sus polos, ceros y ganancia.
- [num, den] =  tfdata (transfer, "vector");
- [Ceros, Polos, Ganancia] =  tf2zp(num, den)
-#6. Mostrar gráficamente la distribución de polos y ceros.
-  pzmap(transfer)
-#7. Indicar estabilidad del sistema.
+ [Ceros, Polos, Ganancia] =  tf2zp(num, den)  
+endfunction
+
+function opcion6(transfer)
+ pzmap(transfer)
+endfunction
+
+function opcion7(transfer)
   estable = isstable(transfer);
   if (estable)
     disp("Es estable!");
   else
     disp("No es estable!");
   endif
+endfunction
 
-#8. Obtener todas las características de la función transferencia: Esta funcionalidad barre todos los puntos anteriores (1, 2, 3, 4, 5,6 y 7).
+function opcion8(transfer)
+  opcion5(transfer)
+  opcion6(transfer)
+  opcion7(transfer)
+endfunction
+
+function opcion9
+  transfer = cargarTF()
+endfunction
+
+
+while(true)
+  opcion = menu ("ASIC!",
+    "1. Obtener la expresión de la función transferencia.",
+    "2. Indicar Polos.",
+    "3. Indicar Ceros.",
+    "4. Marcar ganancia de la función.",
+    "5. Obtener expresión con sus polos, ceros y ganancia.",
+    "6. Mostrar gráficamente la distribución de polos y ceros.",
+    "7. Indicar estabilidad del sistema.",
+    "8. Obtener todas las características de la función transferencia",
+    "9. Ingresar una nueva función.",
+    "10. Finalizar.");
+  switch (opcion)
+    case 1
+     opcion1(transfer)
+    case 2
+     opcion2(transfer)
+    case 3
+     opcion3(transfer)
+    case 4
+     opcion4(transfer)
+    case 5
+     opcion5(transfer)
+    case 6
+     opcion6(transfer)
+    case 7
+     opcion7(transfer)
+    case 8
+     opcion8(transfer)
+    case 9
+     opcion9
+    case 10
+     break;
+  endswitch
+endwhile
